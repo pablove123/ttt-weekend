18ed993d56588940d241e652a5afee3247bf1991
Pablo Villedae <pablov.e12345@gmail.com>
2022-12-11 17:38:59 -0800

    Finished beta 1.0

--- js/app.js
+++ js/app.js
@@ -6,11 +6,8 @@ winningCombos =[[0,1,2],[3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2
 console.log(winningCombos)
 /*---------------------------- Variables (state) ---------*/
 
+let turn = -1
 let board = [null, null,null,null,null,null,null,null,null ]
-console.log(board)
-console.log(squareEls)
-
-let turn = 1
 
 let winner = false
 let tie = false
@@ -20,18 +17,23 @@ let tie = false
 
 
 /*----------------------------- Event Listeners ---------------*/
-  // squareEls.forEach(function (square, a) {
+// squareEls.forEach(function (square, a) {
   //   square.addEventListener('click', handleClick)
   //   const sqIdx = square[a]
-    
+  
   // })
+  document.querySelector("#reset").addEventListener("click", init)
 
-  div.document.querySelector(".board").addEventListener("click", handleClick)
+  document.querySelector(".board").addEventListener("click", handleClick)
 /*-------------------------------- Functions ---------------*/
 init()
 
 function init(){
-  render()
+    board = [null, null, null, null, null, null, null, null, null]
+    turn = 1
+    winner = false
+    tie = false
+    render()
 }
 
 function render(){
@@ -43,26 +45,84 @@ updateMessage()
 function updateBoard(){
   board.forEach( function (ele, i){
     if(ele === 1){
-      squareEls[i].textContent = "X"
-    }  else if( ele === -1) {
       squareEls[i].textContent = "O"
-    } else {
-      squareEls[i].textContent = ""
+    }  else if( ele === -1) {
+      squareEls[i].textContent = "X"
+    } else if (ele === null){
+      squareEls[i].textContent = " "
     }
   }
   )
 }
 
 function updateMessage(){
+  
   if (winner === false && tie === false){
-    return `It is ${turn} turn`
+    if(turn === 1){
+      return document.querySelector("#message").innerHTML= "It is O's turn"} else {
+        return document.querySelector("#message").innerHTML= "It is X's turn"
+      }
   } else if (winner === false && tie === true){
-    return "It is a tie!"
-  } else {
-    return `${turn} has won!`
+    console.log("its a tie")
+    return document.querySelector("#message").innerHTML= "It is a tie! Try Again?"
+  } else if (winner === true){
+    if(turn === 1){
+      return document.querySelector("#message").innerHTML= "X has won!"} else {
+
+      }return document.querySelector("#message").innerHTML= "O has won!"}
   }
+
+
+
+// we have the id of the target thats clicked
+//
+function handleClick(e){
+  // const square = e.target.id
+  // let sqIdx = document.getElementById(square)
+  const test = e.target.id
+  let sqIdx = test.split("").slice(2)
+  if (sqIdx === "X" || sqIdx === "O"){
+    return
+  } else if (winner === true){
+    return
+  } else {
+    placePiece(sqIdx)
+  }
+  render()
+  switchPlayerTurn()
+  checkForWinner(sqIdx)
+  updateMessage()
+  console.log(board)
 }
 
-function handleClick(e){
-  console.log(sqIdx)
+function placePiece(index){
+  board[index] = turn
+  checkForTie()
+}
+
+function checkForTie(){
+  if (board.includes(null) == true) {
+    return
+}else {
+  tie = true
+}
+}
+
+function checkForWinner(){
+    for (let i = 0; i < winningCombos.length; i++) {
+      let sum = board[winningCombos[i][0]] + board[winningCombos[i][1]] + board[winningCombos[i][2]]
+      let absSum = Math.abs(sum)
+      if (absSum === 3) winner = true
+    }
+  }
+
+
+
+function switchPlayerTurn(){
+  if(winner === true){
+    return
+  }else {
+    turn = (turn * -1)
+    console.log ("swicthed turn", turn)
+  }
 }
